db6e9033148bed859cf7343ac27a0cdf7e347b1a
MathiasBOSS <Mathias@aek.no>
2021-05-19 13:28:04 +0200

    Made the login form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Navbar from 'components/Navbar'
+import LoginForm from 'components/LoginForm'
 
 function App() {
-  return <></>;
+  return (
+    <React.Fragment>
+      <Navbar/>
+      <LoginForm/>
+    </React.Fragment>
+  )
 }
 
 export default App;
