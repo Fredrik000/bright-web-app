959c7b72d1666b1a76a9f70d051f39097c68d219
Fredrik000 <52358347+Fredrik000@users.noreply.github.com>
2021-05-20 13:11:44 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import Navbar from 'components/Navbar'
-import LoginForm from 'components/LoginForm'
+import Navbar from 'components/Navbar';
+import LoginForm from 'components/LoginForm';
 import Card from 'components/UI/Card';
 import Form from 'components/Form';
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar/>
-      <LoginForm/>
+      <Navbar />
+      <LoginForm />
       <Card />
       <Form />
     </React.Fragment>
-  )
+  );
+}
 export default App;
