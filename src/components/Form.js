9cf3187f5cb469584f8d17944d05c9db4fa649b5
PernilleOfte <pernilleo1@hotmail.com>
2021-05-21 11:24:13 +0200

    CSS almost done form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,11 @@ import React from 'react';
 function Form() {
   return (
     <div className='form'>
+
       <h1>Repair</h1>
       <p>Small explantation detaling what this is for</p>
-      <form>
+
+        <form>
         <div>
           <label htmlFor='name'></label>
           <input type='text' placeholder='Your name' id='name' />
@@ -13,20 +15,20 @@ function Form() {
         <div>
           <label htmlFor='product'></label>
           <select id='product' >
-              <option value="">Product</option>
+              <option value="" disabled selected>Product</option>
                </select>
         </div>
         <div>
           <label htmlFor='part'></label>
           <select id='part' >
-              <option value="">Part replaced</option> </select>
+              <option value="" disabled selected>Part replaced</option> </select>
         </div>
         <div>
           <label htmlFor='notes'></label>
           <input type='textarea' id='notes' placeholder='Notes'/>
         </div>
       </form>
-      <button>Start</button>
+      <button>Send</button>
     </div>
   );
 }
